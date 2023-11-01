27584c19ecb0199ff218f1f8b85cc1bf3154efbc
moohamedxx <99755972+moohamedxx@users.noreply.github.com>
2023-11-01 19:03:32 +0200

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int x, y;
+//	double answer=0;
+//	char op;
+//	cout << "Please enter the equation" << endl;
+//	cin >> x >> op >> y;
+//	switch (op)
+//	{
+//	case '+':
+//		answer = x + y;
+//		break;
+//	case '-':
+//		answer = x - y;
+//		break;
+//	case '/':
+//		answer = x / y;
+//		break;
+//	case '*':
+//		answer = x * y;
+//		break;
+//	default:
+//		cout << "Wrong data \n";
+//	}
+//
+//	cout << x << " " << op << " " << y << " equals: " << answer << endl;
+//
+//
+//	return 0;
+//}
