ce5bfa5b0800ecbaa78465f6b85a6064593fb7e6
moohamedxx <99755972+moohamedxx@users.noreply.github.com>
2023-11-01 19:11:04 +0200

    Add files via upload

---
+++ numberGussingGame.cpp
@@ -0,0 +1,36 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int x,correctNumber=50;
+//	cout << "please enter a number between 0 and 100 \n";
+//	cin >> x;
+//	while (true)
+//	{
+//		if (x < 0 || x > 100)
+//			cout << "the number is out of range\n";
+//		else
+//			break;
+//	cin >> x;		
+//	}
+//	
+//	while (x != correctNumber)
+//	{
+//		if (x < correctNumber)
+//		{
+//			cout << "the number is greater than yours" << endl;
+//			cin >> x;
+//		}
+//		else
+//		{
+//			cout << "the number is smaller than yours" << endl;
+//			cin >> x;
+//		}
+//	}
+//	cout << "!!! CORRECT GUESS \nTHE CORRECT NUMBER IS " << correctNumber << endl;
+//
+//
+//
+//
+//	return 0;
+//}
